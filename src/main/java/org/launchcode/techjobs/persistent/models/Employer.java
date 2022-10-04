9960a85807fa1283eb7a7c611cbef5c654978ace
Paul Kuhn <pauledwardkuhn@gmail.com>
2022-10-03 20:16:41 -0400

    finished Part 2 to Controllers.  Just started Controllers.

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,24 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+
+@Entity
 public class Employer extends AbstractEntity {
+
+    @NotBlank(message = "You must enter a location")
+    @Size(min = 25)
+    public String location;
+
+    public Employer (){
+
+    }
+
+
+
+
 
 
 }
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,14 @@
 package org.launchcode.techjobs.persistent.models;
 
+
+import javax.persistence.Entity;
+
+@Entity
 public class Skill extends AbstractEntity {
+
+    public String Description;
+    public Skill () {
+
+    }
 
 }
